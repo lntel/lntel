3dd20a7d5f1af607706b501801d49991177beed5
lntel <joeharris461767@gmail.com>
2020-09-14 22:25:25 +0100

    Updates

---
+++ precache-manifest.4fd853142e931374ae1fae3375362c6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10fc27b4f9b9aa574b9a278b0a7ae855",
+    "url": "/lntel/index.html"
+  },
+  {
+    "revision": "0635a454d8216b4ddb8e",
+    "url": "/lntel/static/css/main.e085afb8.chunk.css"
+  },
+  {
+    "revision": "c0cbceccbf472c4b994d",
+    "url": "/lntel/static/js/2.231ad13f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/lntel/static/js/2.231ad13f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0635a454d8216b4ddb8e",
+    "url": "/lntel/static/js/main.c119b6f4.chunk.js"
+  },
+  {
+    "revision": "0cc9aa2763b79089f896",
+    "url": "/lntel/static/js/runtime-main.d4128eb5.js"
+  },
+  {
+    "revision": "fb2b8ff83ed79f1f0b587ecea9f52561",
+    "url": "/lntel/static/media/mountain.fb2b8ff8.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.17d1a16336d8e9330550840b2b2399cd.js"
+  "/lntel/precache-manifest.4fd853142e931374ae1fae3375362c6c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lntel/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
