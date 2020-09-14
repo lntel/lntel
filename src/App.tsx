0501a05d1098fcfb2ba005d2f0e9a69e2144f310
lntel <joeharris461767@gmail.com>
2020-09-14 19:28:19 +0100

    Implemented react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Main from './pages/Main'
+
 const App = () => {
     return (
-        <div>
-            test
-        </div>
+        <Router>
+            <Switch>
+                <Route path='/' exact component={Main} />
+            </Switch>
+        </Router>
     )
 }
 
